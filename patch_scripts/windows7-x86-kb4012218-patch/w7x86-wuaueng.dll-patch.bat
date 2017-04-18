fab83c46a04aedfce93ddd4a42a243e71022d7a8
neroux <neroux@users.noreply.github.com>
2017-04-18 23:44:07 +0200

    Added OS version check

--- patch_scripts/windows7-x86-kb4012218-patch/w7x86-wuaueng.dll-patch.bat
+++ patch_scripts/windows7-x86-kb4012218-patch/w7x86-wuaueng.dll-patch.bat
@@ -9,6 +9,11 @@ net session >nul 2>&1 || (
 echo Checking system requirements...
 echo.
 
+wmic /output:stdout os get version | find "6.1" > nul || (
+    echo Detected that you are not running Windows 7. Please verify that
+    echo you are trying to install the right patch file and try again.
+)
+
 wmic /output:stdout qfe get hotfixid | find "KB4012218" >nul || (
     echo Detected that update KB4012218 is not installed, please verify that
     echo you are trying to install the right patch file and try again.
